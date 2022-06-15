15af556f1b24534dfc727deeb6aafb920299f893
wcy <73599280+1754490451@users.noreply.github.com>
2022-06-15 15:29:49 +0800

    设计模式:单例模式

--- sheJiMoShi/src/main/java/com/example/singleton/Hungry.java
+++ sheJiMoShi/src/main/java/com/example/singleton/Hungry.java
@@ -2,11 +2,27 @@ package com.example.singleton;
 
 
 /**
- *
+ * 饿汉式
+ * 类加载到内存后，就实例化一个单例，JVM保证线程安全
+ * 简单实用，推荐！
+ * 唯一缺点：不管用不用得到，类加载的时候就完成实例化
  *
  */
 public class Hungry {
+    private static final Hungry INSTANCE = new Hungry();
+
+    private Hungry() {
+    }
+
+    public static Hungry getInstance(){
+        return INSTANCE;
+    }
 
 
+    public static void main(String[] args) {
+        Hungry instance = Hungry.getInstance();
+        Hungry instance1 = Hungry.getInstance();
+        System.out.println(instance == instance1);
+    }
 
 }
---
+++ sheJiMoShi/src/main/java/com/example/singleton/Lazy.java
@@ -0,0 +1,13 @@
+package com.example.singleton;
+
+
+/**
+ * 饱汉式（懒加载）
+ *
+ *
+ */
+public class Lazy {
+
+
+
+}
